3c39685fd8ed3f30acec69d5333e514adcba53dc
henrydavis <>
2019-11-30 11:56:33 +1100

    Initial Commit

---
+++ src/main/java/com/belong/phone/number/service/Application.java
@@ -0,0 +1,13 @@
+package com.belong.phone.number.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
